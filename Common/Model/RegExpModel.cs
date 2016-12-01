790a93d876d8c4e2402672c5f7118205d5eb1d8b
逍遥帝君 <@DESKTOP-282ALGR>
2016-12-01 20:09:58 +0800

    添加项目文件。

---
+++ Common/Model/RegExpModel.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace Common.Model
+{
+    /// <summary>
+    /// 正则表达式
+    /// </summary>
+    public class RegExpModel
+    {
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 中文名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 英文名称
+        /// </summary>
+        public string UniueName { get; set; }
+        /// <summary>
+        /// 正则内容
+        /// </summary>
+        public string RegexContent { get; set; }
+    }
+}
